3f9790acdd01bf83f14ae53e523f646a7dee8a9d
Gunpreet Singh <gunpreet@outlook.com>
2017-06-19 11:56:22 -0700

    add app component

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js?/,
+        test: /\.jsx?/,
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
