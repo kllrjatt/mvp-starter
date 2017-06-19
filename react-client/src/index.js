49088c9f84eb9fdf8800433378fd1152cbd5d812
Gunpreet Singh <gunpreet@outlook.com>
2017-06-19 13:32:16 -0700

    fix comments

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,7 +1,11 @@
+// import react , react dom, jquerry and app 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import App from './App.jsx'
 
+
+// render app in slef closing tag 
 
 ReactDOM.render(<App />, document.getElementById('app'))
