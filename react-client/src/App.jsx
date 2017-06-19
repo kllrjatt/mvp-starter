9e425537528af659443a073380d586919b45641b
Gunpreet Singh <gunpreet@outlook.com>
2017-06-19 12:11:59 -0700

    add place holder sections for Spotify API call

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -6,8 +6,25 @@ class App extends Component {
   // add render component 
   render() {
     // add return function and add div tag to it with some text
+    // add title, search bar, submit button 
+    // add artist picture, name and gallery 
     return (
-      <div>All Music Search</div>
+      <div>
+        <div className='title'>
+          All Music Search
+      </div>
+        <div>
+          <input placeholder='Search for Your Artist Here' />
+          <button>Search!</button>
+        </div>
+        <div className='artistProfile'>
+          <div>Picture Goes Here</div>
+          <div>Name Goes Here</div>
+        </div>
+        <div className='music'>
+          Music Goes Here
+        </div>
+      </div>
     )
   }
 }
