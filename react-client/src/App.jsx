d41196bfcc434355468259e71a986bc80150d4e5
Gunpreet Singh <gunpreet@outlook.com>
2017-06-19 20:39:43 -0700

    working on spotify api

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 // add css file 
 import './style.css';
 
+
 // import multiple items from react - bootstrap 
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
@@ -16,6 +17,25 @@ class App extends Component {
     }
   }
 
+  search() {
+    // add url for search
+    var url = 'https://api.spotify.com/v1/search?';
+    var searchUrl = `${url}q=${this.state.query}&type=artist&limit=1`
+    // use postman to add settings to the search function with token and cache 
+    var options = {
+      "async": true,
+      "crossDomain": true,
+      "url": this.searchUrl,
+      "method": "GET",
+      "headers": {
+        "authorization": "Bearer BQCRW7PfM9R1h0TyB2HeeNKLKiGubRM6osif_fLCMHVG7pmsmvbZrCWcyPoJROooIOCFJCThoHCns0KSubgviBJK0ICWrUT0Y0nE7u_TdSngpLeBupnZDoiD9mP5Xh3_8ncpodOsH3C2bkcCah47ZtNziYJAzfN8hiicXWO4ru0nDwCEcXkHUnZaapVd5ezZ5JWp",
+        "cache-control": "no-cache"
+      }
+    }
+    // add fetch function 
+    fetch(options).then(response => console.log(response))
+      // .then(json => console.log(json))
+  }
 
   // add render component 
   render() {
@@ -31,9 +51,18 @@ class App extends Component {
       </div>
         <FormGroup>
           <InputGroup>
-            <FormControl type='text'
-              placeholder='Search for a Artist' />
-            <InputGroup.Addon>
+            <FormControl
+              type='text'
+              placeholder='Search for a Artist'
+              value={this.state.query}
+              onChange={event => { this.setState({ query: event.target.value }) }}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph='search'></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
@@ -51,3 +80,11 @@ class App extends Component {
 }
 
 export default App
+
+
+// add query field to search 
+  // add a onchange event 
+    // use on change event to set the new state 
+    // state will be the value of the event aka the value of the input bar
+  // invoke the search function on the input field
+  // use onkeypress event.key = enter to invoke the search 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var items = require('../database-mongo');
+var request = require("request");
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.get('/items', function (req, res) {
     }
   });
 });
+
 
 app.listen(3000, function () {
   console.log('listening on port 3000!');
